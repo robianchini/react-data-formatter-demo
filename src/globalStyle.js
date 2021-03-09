1a268501f40c9d772eb37a857d3e74231f2444da
Rodrigo Bianchini <rodrigo.bianchinii@gmail.com>
2021-03-08 21:51:53 -0300

    publish

---
+++ src/globalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Montserrat', sans-serif;
+    background-color: #fff;
+    overflow-x: hidden;
+    padding: 0;
+    margin: 0;
+    scroll-behavior: smooth;
+    box-sizing: border-box;
+  }
+`
