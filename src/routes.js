36ed14df828c9209c7829e8231687d64eda46715
Rodrigo Bianchini <35143346+robianchini@users.noreply.github.com>
2021-03-09 14:24:08 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 const Routes = () => (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
     </Switch>
